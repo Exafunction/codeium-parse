19aa8de4a140978b89793b6e1a83a4c0f1d3aa85
Prem Nair <prem@exafunction.com>
2023-03-09 15:11:55 -0800

    Project import generated by Copybara. (#9)
    GitOrigin-RevId: c44aa3ca6ca6ce1bc8134c46867008f0082f16e3
    
    Co-authored-by: Copybara Bot <copybara@exafunction.com>

--- queries/python_tags.scm
+++ queries/python_tags.scm
@@ -1,13 +1,19 @@
 ;; 0. Pattern 0 from tags.scm.
-(class_definition
-  name: (identifier) @name
-  body: (block . (expression_statement . (string) @doc .)?)) @definition.class
+(
+  (class_definition
+    name: (identifier) @name
+    body: (block . (expression_statement . (string) @doc .)?)) @definition.class
+  (#not-has-parent? @definition.class decorated_definition)
+)
 
 ;; 1. Pattern 1 from tags.scm.
-(function_definition
-  name: (identifier) @name
-  parameters: (parameters) @codeium.parameters
-  body: (block . (expression_statement . (string) @doc .)?)) @definition.function
+(
+  (function_definition
+    name: (identifier) @name
+    parameters: (parameters) @codeium.parameters
+    body: (block . (expression_statement . (string) @doc .)?)) @definition.function
+  (#not-has-parent? @definition.function decorated_definition)
+)
 
 ;; 2. Pattern 2 from tags.scm.
 ;; Note that we can't distinguish from @reference.class.
@@ -17,3 +23,18 @@
       (attribute
         attribute: (identifier) @name)
   ]) @reference.call
+
+(
+  (decorated_definition
+    definition: (class_definition
+      name: (identifier) @name
+      body: (block . (expression_statement . (string) @doc .)?))) @definition.class
+)
+
+(
+  (decorated_definition
+    definition: (function_definition
+      name: (identifier) @name
+      parameters: (parameters) @codeium.parameters
+      body: (block . (expression_statement . (string) @doc .)?))) @definition.function
+)
--- test_files/test.py
+++ test_files/test.py
@@ -1,5 +1,7 @@
 # fmt: off
 
+from foo import decorated1
+from foo import decorated2
 from foo import Pattern2a
 from foo import pattern2b
 
@@ -20,5 +22,19 @@ def pattern1(param1):
 Pattern2a()
 pattern2b(1)
 Pattern2a.foo()
+
+@decorated1
+@decorated2
+class DecoratedClass:
+    """Decorated class docstring."""
+    @decorated1
+    @decorated2
+    def decorated_method(self):
+        """Decorated method docstring."""
+
+@decorated1
+@decorated2
+def decorated_function():
+    """Decorated function docstring."""
 
 # fmt: on
